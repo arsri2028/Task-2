8ed5d727bd57c7ecb044ac4b8ef624e9f17d3eac
Arunima Chandra Verma <85626464+arsri2028@users.noreply.github.com>
2021-07-30 19:14:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,52 @@
+import Users from "./Components/Cards";
+import './App.css';
+
+import React, { Component } from 'react'
+
+class App extends Component {
+   constructor(props) {
+   super(props)
+
+   // Set initial state
+   this.state = {users_data :[],
+                 loading : true
+}
+
+
+   this.updatestate = this.updatestate.bind(this)
+   }
+
+   updateState(){
+        // document.getElementById("main").style.display='flex';
+        const link="https://reqres.in/api/users?page=1";
+        fetch(link)
+        .then(response => response.json())
+        .then((users) => {
+
+           this.setState({users_data :users.data,
+                           loading: false
+            })
+            console.log(users.data);
+          })
+          .catch((error) => {
+             console.error(error)
+            })
+    }
+    
+    render(){
+       return 
+         <navigator>
+              <div className="box">
+               <div className="row">
+               <div className="column1">
+               <h2>Lets Grow More</h2> 
+               </div>
+               <div className="column2">
+               <button onClick={this.updateState}>Get Users</button> 
+               </div>    
+               </div>
+               </div>
+              </navigator>
+      }
+     }
+     export default App;
